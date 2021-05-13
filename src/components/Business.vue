93e6db6a8011d85c1920d64b7331a38f4135f843
Vlad <juzopigeon@gmail.com>
2021-05-14 00:27:24 +0600

    Add files via upload

---
+++ src/components/Business.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>hi</p>
+</template>
+
+<script>
+
+export default {
+  name: 'Business',
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/House.vue
@@ -0,0 +1,440 @@
+<template>
+
+       <section class="house-intro">
+         <div class="flex">
+           <h2>Системы бесперебойного электроснабжения </h2>
+           <p>Предотврати проблемы с электроснобжением умно и <br> эффективно</p>
+
+         </div>
+          
+          <img  src="../assets/house/sun-house.png" alt="">
+            <a href="#" class="btn">Подробнее</a> 
+       </section>
+      
+         
+
+       <section class="problems">
+         <div><h2>ПРОБЛЕМЫ  С ЭЛЕКТРОСНАБЖЕНИЕМ:</h2>
+          <img src="../assets/house/image-1.png" alt="">
+          </div>
+
+         
+         <div class="f-para">
+           
+        <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+           
+          <p>Пропадание напряжение</p>
+          <p>Провал/всплеск напряжения</p>
+          <p>Понижение/повышение 
+напряжения</p>
+          <p>Электромагнитные<br>
+и радиочастотные помехи</p></div>
+        
+        
+       </section>
+
+       <section class="complex">
+          <h2>Комплексные системы 
+бесперебойного электроснабжения</h2>
+
+<div class="co-contain">
+   <div>
+     <img src="../assets/house/light.png" alt=""> 
+     <img src="../assets/house/sun.png" alt=""> 
+     <p>Сетевые 
+Солнечные Элекстростанции</p>
+</div>
+   <div>
+     <img src="../assets/house/waves.png" alt=""> 
+     <img src="../assets/house/white-leafe.png" alt=""> 
+     <p>Источник 
+Бесперебойного Питания</p>
+</div>
+   <div>
+     <img src="../assets/house/leafe.png" alt=""> 
+     <img src="../assets/house/vec-1.png" alt=""> 
+     <p>Аккумуляторные Батареи </p></div>
+   <div>
+     <img src="../assets/house/lightning.png" alt=""> 
+     <img src="../assets/house/earth.png" alt=""> <p>Генераторные установки (дизель, газ) </p></div>
+   <div>
+     <img src="../assets/house/circ-light.png" alt=""> 
+     <img src="../assets/house/drop.png" alt=""> <p>Устройства Защиты 
+от Импульсных Перенапряжений </p></div>
+   <div>
+     <img src="../assets/house/circle.png" alt=""> 
+     <img src="../assets/house/hearted-seed.png" alt=""> <p>Устройства
+ Защитного Отключения  </p></div>
+</div>
+       </section>
+       
+     
+      
+       <section class="generate">
+              <h2>1. Замена Генератора</h2>
+         <div class="f-para f-para-1">
+          <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+            </div> 
+            
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+       
+        </div>
+        <img src="../assets/house/generator.png" alt="">
+       </section>
+
+
+
+       <section class="auto auto-2">
+              <h2>2. Отдельный автомат</h2>
+         <div class="f-para f-para-1">
+          <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+            </div> 
+            
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+       
+        </div>
+        <img src="../assets/house/automate.png" alt="">
+       </section>
+
+
+           <section class="auto auto-3">
+              <h2>3. Одна фаза из трех</h2>
+         <div class="f-para f-para-1 float">
+          <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+            </div> 
+            
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+       
+        </div>
+        <img src="../assets/house/build.png" alt="">
+       </section>
+
+           <section class="auto  auto-4">
+              <h2>4. Электропитание на весь дом</h2>
+         <div class="f-para f-para-1 float">
+          <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+            </div> 
+            
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+       
+        </div>
+        <img src="../assets/house/image-4.png" alt="">
+       </section>
+
+
+           <section class="auto auto-5">
+              <h2>5. ИБП + Генератор</h2>
+         <div class="f-para f-para-1 float">
+          <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+            </div> 
+            
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+       
+        </div>
+        <img src="../assets/house/image-13.png" alt="">
+       </section>
+
+
+           <section class="auto auto-6">
+              <h2>6. Сетевая солнечная электростанция</h2>
+         <div class="f-para f-para-1 float">
+          <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+            </div> 
+            
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+       
+        </div>
+        <img src="../assets/house/sun-light.png" alt="">
+       </section>
+
+           <section class="auto auto-7">
+              <h2>7. Городская и офисная инфраструктура</h2>
+         <div class="f-para f-para-1 float">
+          <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+            </div> 
+            
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+              <div>
+            <h3>Надежность и долговечность</h3>
+             <p>Правильность заземления<br>
+и защита розеточных групп </p>
+
+            </div> 
+       
+        </div>
+        <img src="../assets/house/image-15.png" alt="">
+       </section>
+
+       
+
+
+      
+       
+      
+ 
+
+</template>
+
+<script>
+
+export default {
+  name: 'House',
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.auto  {
+  max-width: 1100px;
+  margin: auto;
+}
+
+.auto img {
+  bottom: 1000px;
+  height: 400px;
+}
+
+
+.generate h2 {
+  margin-bottom: 20px;
+  margin-right: 650px;
+}
+
+.auto h2 {
+  margin-bottom: 20px;
+  margin-right: 650px;
+}
+.generate  {
+  max-width: 1100px;
+  margin: auto;
+}
+.generate img {
+  height: 400px;
+}
+.f-para-1 {
+  text-align: left;
+  margin-left: 100px;
+}
+
+
+
+.co-contain {
+  margin-top: 20px;
+  justify-content: center;
+  margin: auto;
+  align-items: center;
+  max-width: 1000px;
+  grid-gap: 90px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+
+.co-contain {
+  margin-top: 20px;
+}
+ 
+.f-para {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-bottom: 600px;
+
+}
+.f-para p {
+
+  margin-top: 50px;
+  
+}
+
+
+.btn {
+  text-align: center;
+ padding-top: 20px;
+  display: block;
+  background:#62B82D;
+  width: 250px;
+  height: 60px;
+  color: rgb(240, 232, 232);
+  
+}
+  
+ .house-intro {
+   color: white;  
+ }
+
+ .flex {
+   text-align: left;
+   display: flex;
+   flex-direction: column;
+ }
+
+ 
+
+
+ .problems {
+   margin: auto;
+   display: grid;
+   justify-content: center;
+   grid-template-columns: 700px 400px;
+   grid-template-rows: 1000px 1000px;
+   margin-top: 20px;
+   
+  
+ }
+ 
+  .problems img {
+    height: 400px;
+    margin-top: 20px;
+    
+   
+     
+  }
+
+  .sqr {
+    width: 120px;
+height: 120px;
+background: #62B82D;
+
+  }
+
+  
+
+</style>
